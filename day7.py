05b9018084836181b4b254dcf1cc40fb34593700
Tom Cook <tomcook@Toms-Laptop.local>
2023-01-03 01:36:49 -0500

    day 7 and debug config

--- day7.py
+++ day7.py
@@ -18,21 +18,67 @@ class FileNode:
             return fileSize
         else:
             return fileSize + sum([fn.total_size() for fn in self.childNodes])
+    
+    def find_subdirs(self, threshold: int):
+        subdirs = []
+        searchNodes = self.childNodes
+        while len(searchNodes) > 0:
+            mnode = searchNodes.pop()
+            if mnode.total_size() <= threshold:
+                subdirs.append(mnode)
+            searchNodes.extend(mnode.childNodes)
+        return subdirs
 
+    def find_smallest_above(self, required: int):
+        searchNodes = self.childNodes
+        dirs_above = []
+        while len(searchNodes) > 0:
+            mnode = searchNodes.pop()
+            if mnode.total_size() < required:
+                continue
+            else:
+                dirs_above.append(mnode)
+            searchNodes.extend(mnode.childNodes)
+        min_bytes_over = float('inf')
+        print(dirs_above)
+        final_node = None
+        for d in dirs_above:
+            bytes_over = d.total_size() - required
+            print(d, bytes_over)
+            if bytes_over < min_bytes_over:
+                min_bytes_over = bytes_over
+                final_node = d
+        return final_node.total_size()
+
+    def __str__(self) -> str:
+        return self.name + ', size=' + str(self.total_size())
+    
+    def __repr__(self) -> str:
+        return self.name + ', size=' + str(self.total_size())
+
+    def print_tree(self, numTabs: int):
+        dir_str = ' ' * numTabs
+        print(dir_str + '-' + self.name)
+        for f in self.files:
+            print(dir_str + '-' + f.name + ' (file, size=' + str(f.size) + ')')
+        for f in self.childNodes:
+            f.print_tree(numTabs+1)
 
 file = open("day7_input.txt", 'r')
-path = ''
+path = '/'
 root_node = FileNode(path, None, [], [])
 current_node = root_node
 
 for line in file.readlines():
     if line.startswith("$"):
         commandArgs = line.rstrip().split(" ")
-        command = commandArgs[0]
+        command = commandArgs[1]
         if command == 'cd':
-            newPath = commandArgs[1]
+            newPath = commandArgs[2]
             if newPath == '..':
                 current_node = current_node.parent
+            elif newPath == root_node.name:
+                continue
             else:
                 next_node = [f for f in current_node.childNodes if f.name == newPath]
                 current_node = next_node[0]
@@ -42,7 +88,7 @@ for line in file.readlines():
         file_listing = line.rstrip().split(" ")
         if file_listing[0] == 'dir':
             nodeName = file_listing[1]
-            node = FileNode(current_node.name + nodeName, current_node, [], [])
+            node = FileNode(nodeName, current_node, [], [])
             current_node.childNodes.append(node)
         else:
             fileName = file_listing[1]
@@ -51,7 +97,12 @@ for line in file.readlines():
             current_node.files.append(newFile)
 
 threshold = 100000
-
-search_node = root_node
-
-
+# dires = root_node.find_subdirs(threshold)
+# print(sum([f.total_size() for f in dires]))
+root_node.print_tree(0)
+available = 70000000
+update = 30000000
+unused = available - root_node.total_size()
+required = update - unused
+print(required)
+print(root_node.find_smallest_above(required))
